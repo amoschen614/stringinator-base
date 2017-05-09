cf6e3aaaf31849c6836176062f5c0c90954f1970
Amos Chen <amoschen614@gmail.com>
2017-05-09 00:41:59 -0700

    add failing tests for .contains for both objects and arrays

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -2,19 +2,29 @@ const _ = require('../../underbar');
 
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
-    // Your code goes here
+    const arr = [1, 2, 3];
+    expect(_.contains(arr, 2)).toBe(true);
   });
 
   it('returns false if an array does not contain the target', () => {
-    // Your code goes here
+    const arr = [1, 3];
+    expect(_.contains(arr, 2)).toBe(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
-    // Your code goes here
+    const obj = {
+      one: 1,
+      two: 2,
+    };
+    expect(_.contains(obj, 2)).toBe(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
-    // Your code goes here
+    const obj = {
+      one: 1,
+      two: 2,
+    };
+    expect(_.contains(obj, 3)).toBe(false);
   });
 
 });
