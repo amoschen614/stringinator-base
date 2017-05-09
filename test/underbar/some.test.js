d83c92608b216c269903ec31202ba21a996f695a
Amos Chen <amoschen614@gmail.com>
2017-05-09 09:54:16 -0700

    add failing tests for .some

--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -2,11 +2,13 @@ const _ = require('../../underbar');
 
 describe('some()', () => {
   it('returns true if any number is odd', () => {
-    // Your code goes here
+    const arr = [2, 3, 4];
+    expect(_.some(arr, el => el % 2 !== 0)).toBe(true);
   });
 
   it('returns false if no number is odd', () => {
-    // Your code goes here
+    const arr = [2, 4];
+    expect(_.some(arr, el => el % 2 !== 0)).toBe(false);
   });
 
 });
