2298eec5209aa70b8eadb570ef94ea098509c558
Amos Chen <amoschen614@gmail.com>
2017-05-09 10:36:34 -0700

    add failing tests for .reject

--- test/underbar/reject.test.js
+++ test/underbar/reject.test.js
@@ -2,10 +2,14 @@ const _ = require('../../underbar');
 
 describe('reject()', () => {
   it('rejects odd numbers from an array', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4, 5];
+    const evens = _.reject(arr, el => el % 2 !== 0);
+    expect(evens).toEqual([2, 4]);
   });
 
   it('rejects null values from an object', () => {
-    // Your code goes here
+    const arr = [1, null, 2, null, 3];
+    const nums = _.reject(arr, el => el === null);
+    expect(nums).toEqual([1, 2, 3]);
   });
 });
