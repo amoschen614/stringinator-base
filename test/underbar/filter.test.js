76d3aec300e28a38413dd4597dc75309141895bb
Amos Chen <amoschen614@gmail.com>
2017-05-09 10:04:45 -0700

    add failing tests for .filter

--- test/underbar/filter.test.js
+++ test/underbar/filter.test.js
@@ -2,10 +2,14 @@ const _ = require('../../underbar');
 
 describe('filter()', () => {
   it('filters an array to odd numbers', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4, 5];
+    const odds = _.filter(arr, el => el % 2 !== 0);
+    expect(odds).toEqual([1, 3, 5]);
   });
 
   it('filters an object to only numeric values', () => {
-    // Your code goes here
+    const arr = [1, 'a', 2, 'b', 3];
+    const nums = _.filter(arr, el => typeof el === 'number');
+    expect(nums).toEqual([1, 2, 3]);
   });
 });
