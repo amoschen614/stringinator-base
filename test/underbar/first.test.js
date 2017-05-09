140acb0057a1d3cbd9322749f8d2198ee96b89da
Amos Chen <amoschen614@gmail.com>
2017-05-08 20:34:43 -0700

    add failing tests for .first

--- test/underbar/first.test.js
+++ test/underbar/first.test.js
@@ -2,14 +2,17 @@ const _ = require('../../underbar');
 
 describe('first()', () => {
   it('returns the first element of an array', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4, 5];
+    expect(_.first(arr)).toBe(1);
   });
 
   it('returns the first 2 elements of an array', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c'];
+    expect(_.first(arr, 2)).toEqual(['a', 'b']);
   });
 
   it('returns the whole array if you ask for more elements than it has', () => {
-    // Your code goes here
+    const arr = [1, 2, 3];
+    expect (_.first(arr, 4)).toEqual([1, 2, 3]);
   });
 });
