025468d2b96b69ece2cee38484e0f54c86bd2f55
Amos Chen <amoschen614@gmail.com>
2017-05-08 22:35:04 -0700

    add failing test for .map

--- test/underbar/map.test.js
+++ test/underbar/map.test.js
@@ -2,6 +2,8 @@ const _ = require('../../underbar');
 
 describe('map()', () => {
   it('maps every numbers in an array of numbers to their square', () => {
-    // Your code goes here
+    const roots = [1, 2, 3, 4, 5];
+    const squares = _.map(roots, el => el * el);
+    expect(squares).toEqual([1, 4, 9, 16, 25]);
   });
 });
