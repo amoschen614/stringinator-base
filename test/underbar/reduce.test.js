898243865546f926bdf27d443871b778fea38029
Amos Chen <amoschen614@gmail.com>
2017-05-08 22:58:32 -0700

    add failing tests for reduce with and without explicit accumulator starting value

--- test/underbar/reduce.test.js
+++ test/underbar/reduce.test.js
@@ -2,11 +2,13 @@ const _ = require('../../underbar');
 
 describe('reduce()', () => {
   it('reduces an array of numbers to a sum, *with* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    const arr = [1, 2, 3];
+    expect(_.reduce(arr, (a, el) => a + el, 4)).toBe(10);
   });
 
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    const arr = [1, 2, 3];
+    expect(_.reduce(arr, (a, el) => a + el)).toBe(6);
   });
 
 });
