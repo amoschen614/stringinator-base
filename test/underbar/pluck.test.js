57d02d108745536fab50371f6d88431354423887
Amos Chen <amoschen614@gmail.com>
2017-05-08 22:50:20 -0700

    add test for .pluck

--- test/underbar/pluck.test.js
+++ test/underbar/pluck.test.js
@@ -2,7 +2,17 @@ const _ = require('../../underbar');
 
 describe('pluck()', () => {
   it('returns an array of just ages, given an array of people objects', () => {
-    // Your code goes here
+    const people = [
+      { 
+        name: 'dp',
+        age: 16
+      },
+      { name: 'felix',
+        age: 99
+      }
+    ];
+    const ages = _.pluck(people, 'age');
+    expect(ages).toEqual([16, 99]);
   });
 
 });
