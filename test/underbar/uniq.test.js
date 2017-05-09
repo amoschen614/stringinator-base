09ed36368758e4aa05e22908e8ccd1239583b3ea
Amos Chen <amoschen614@gmail.com>
2017-05-09 10:51:10 -0700

    add failing tests for .uniq

--- test/underbar/uniq.test.js
+++ test/underbar/uniq.test.js
@@ -2,6 +2,7 @@ const _ = require('../../underbar');
 
 describe('uniq()', () => {
   it('de-dups a list of numbers', () => {
-    // Your code goes here
+    const arr = [1, 2, 2, 1, 3];
+    expect(_.uniq(arr)).toEqual([1, 2, 3]);
   });
 });
