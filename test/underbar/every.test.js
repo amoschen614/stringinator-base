62f0ddbe7cc00749aa9d35e548932e03f7e277fd
Amos Chen <amoschen614@gmail.com>
2017-05-09 00:52:21 -0700

    add failing tests for .every

--- test/underbar/every.test.js
+++ test/underbar/every.test.js
@@ -3,15 +3,20 @@ const _ = require('../../underbar');
 describe('every()', () => {
   describe('processing an array of numbers', () => {
     it('will return true if no callback is supplied', () => {
-      // Your code goes here
+      const arr = [1, 2, 3];
+      expect(_.every(arr)).toBe(true);
     });
 
     it('returns true if all numbers in an array are odd and we test for odd numbers', () => {
-      // Your code goes here
+      const arr = [1, 3, 5];
+      const areOdd = _.every(arr, el => el % 2 !== 0);
+      expect(areOdd).toBe(true);
     });
 
     it('returns false if not all numbers in an array are odd and we test for odd numbers', () => {
-      // Your code goes here
+      const arr = [1, 2, 3];
+      const areOdd = _.every(arr, el => el % 2 !== 0);
+      expect(areOdd).toBe(false); 
     });
 
   });
