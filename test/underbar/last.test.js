fef71cce164cd38da7479ea013cb89dfa7cd6fbb
Amos Chen <amoschen614@gmail.com>
2017-05-08 20:51:03 -0700

    add failing tests for .last

--- test/underbar/last.test.js
+++ test/underbar/last.test.js
@@ -2,14 +2,17 @@ const _ = require('../../underbar');
 
 describe('last()', () => {
   it('returns the last element of an array', () => {
-    // Your code goes here
+    const arr = [1, 2, 3];
+    expect(_.last(arr)).toBe(3);
   });
 
   it('returns the last 2 elements of an array', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c'];
+    expect(_.last(arr, 2)).toEqual(['b', 'c']);
   });
 
   it('returns the whole array if you ask for more elements than it has', () => {
-    // Your code goes here
+    const arr = [1, 2, 3];
+    expect(_.last(arr, 4)).toEqual(arr);
   });
 });
