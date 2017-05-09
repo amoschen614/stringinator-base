c2abf06dc54cb2082b6a96bb221102866c42ccc3
Amos Chen <amoschen614@gmail.com>
2017-05-08 21:17:57 -0700

    add failing tests for .isArrayLike

--- test/underbar/isArrayLike.test.js
+++ test/underbar/isArrayLike.test.js
@@ -2,14 +2,25 @@ const _ = require('../../underbar');
 
 describe('isArrayLike()', () => {
   it('returns true for an actual array', () => {
-    // Your code goes here
+    const arr = [1, 2, 3];
+    expect(_.isArrayLike(arr)).toBe(true);
   });
 
   it('returns true for an array-like object', () => {
-    // Your code goes here
+    const obj = {
+      length: 3,
+      1: 'a',
+      2: 'b',
+      3: 'c'
+    };
+    expect(_.isArrayLike(obj)).toBe(true);
   });
 
   it('returns false for a non-array-like object', () => {
-    // Your code goes here
+    const obj = {
+      name: 'Duncan',
+      type: 'dog'
+    };
+    expect(_.isArrayLike(obj)).toBe(false);
   });
 });
