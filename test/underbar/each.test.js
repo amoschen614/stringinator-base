2261c5eb085223349d02ab58546c0f9be86ec7a1
Amos Chen <amoschen614@gmail.com>
2017-05-08 21:52:52 -0700

    add failing tests for .each

--- test/underbar/each.test.js
+++ test/underbar/each.test.js
@@ -2,15 +2,40 @@ const _ = require('../../underbar');
 
 describe('each()', () => {
   it('iterates every element of an array, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const arr = [1, 2, 3];
+    let count = 0;
+    _.each(arr, el => {
+      expect(el).toBe(arr[count++]);
+    })
+    expect(count).toBe(arr.length);
   });
 
   it('iterates every element of an array-like object, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const obj = {
+      length: 3,
+      1: 'a',
+      2: 'b',
+      3: 'c', 
+    };
+    let count = 0;
+    _.each(obj, prop => {
+      expect(prop).toBe(obj[count++]);
+    });
+    expect(count).toBe(obj.length);
   });
 
   it('iterates every property of an object, passing the value, the corresponding key, and the entire object to the callback', () => {
-    // Your code goes here
+    const obj = {
+      name: 'Duncan',
+      type: 'dog',
+      isPet: true,
+    };
+    let count = 0;
+    _.each(obj, (val, key, obj) => {
+      expect(val).toBe(obj[key]);
+      count++;
+    });
+    expect(count).toBe(3);
   });
 });
 
