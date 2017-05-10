52d47c178ee3d2fef9af91e7e7159c6f013d3c39
Amos Chen <amoschen614@gmail.com>
2017-05-09 17:22:34 -0700

    implement .isOnlyDigits to pass all tests

--- stringinator.js
+++ stringinator.js
@@ -17,7 +17,7 @@ const hasChar = function(str, target) {
 };
 
 const isOnlyDigits = function(str) {
-  // Your code goes here
+  return _.every(str, ch => !isNaN(parseInt(ch)));
 };
 
 const filterToOnlyDigits = function(str) {
